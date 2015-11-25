363ed8e565db057f1fa5c156f8cec3b5eb3b126a
Rainer Bendig <hexathos@gmail.com>
2015-11-25 22:11:31 +0100

    wip

--- src/Monolog/ElasticLogstashHandler.php
+++ src/Monolog/ElasticLogstashHandler.php
@@ -33,7 +33,7 @@ class ElasticLogstashHandler extends \Monolog\Handler\AbstractProcessingHandler
             $this->client->index(
                 [
                     'index' => $this->options['index'],
-                    'type' => $this->options['type']
+                    'type' => $this->options['type'],
                     'body' => json_decode($record['formatted'], true)
                 ]
             );
